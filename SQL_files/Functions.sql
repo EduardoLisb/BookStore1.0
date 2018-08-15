1dbdadc45e669b4d3cebe7c0a73667a9a879c71d
saulolks <saulolucas.gm@gmail.com>
2018-08-15 00:46:11 -0300

    Function TipoEvento

--- SQL_files/Functions.sql
+++ SQL_files/Functions.sql
@@ -9,7 +9,7 @@ begin	DECLARE desconto double;
         PF.cod_plano = P.cod_plano;
         SET desconto = desconto/100;
 		return desconto;
-end//
+END//
 
 CREATE DEFINER=`root`@`localhost` FUNCTION `cepEstado`(cpfCliente bigint) RETURNS varchar(20) CHARSET latin1
 BEGIN
@@ -20,4 +20,19 @@ BEGIN
     C.cpf = P.cpf AND
     P.endereco_cep >= cE.cepInicio AND P.endereco_cep <= cE.cepFim;
     return estado;
-END
+END//
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `tipoEvento`(cpfCliente bigint) RETURNS int(5)
+BEGIN
+	declare tipo integer(5);
+    SELECT count(*) AS total, T.cod_tipo INTO tipo
+    FROM cliente C, inscricao I, evento E, tipo_evento T 
+    WHERE cpfCliente = C.cpf AND
+    C.cpf = I.cpf AND
+    E.cod_evento = I.cod_evento AND
+    T.cod_tipo = E.cod_tipo
+    GROUP BY T.cod_tipo
+    ORDER BY count(*)
+    LIMIT 0,1;
+    return tipo;
+END//
